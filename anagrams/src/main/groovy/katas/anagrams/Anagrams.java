20fb7f35ba842ca68cfbb517539affdf60592b1b
Rob Fletcher <rfletcher@netflix.com>
2015-07-28 12:43:20 -0500

    anagram kata

---
+++ anagrams/src/main/groovy/katas/anagrams/Anagrams.java
@@ -0,0 +1,10 @@
+package katas.anagrams;
+
+import java.util.Set;
+import java.util.function.Function;
+
+public class Anagrams implements Function<String, Set<String>> {
+  @Override public Set<String> apply(String s) {
+    return null;
+  }
+}
---
+++ anagrams/src/test/groovy/katas/anagrams/AnagramsSpec.groovy
@@ -0,0 +1,10 @@
+package katas.anagrams
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+class AnagramsSpec extends Specification {
+
+  @Subject def anagrams = new Anagrams()
+
+}
