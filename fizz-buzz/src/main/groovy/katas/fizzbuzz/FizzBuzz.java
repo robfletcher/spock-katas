0944275642de3cd03f72e04444c60c2e598b2550
Rob Fletcher <rfletcher@netflix.com>
2015-07-28 11:18:24 -0500

    added fizz buzz kata

---
+++ fizz-buzz/src/main/groovy/katas/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,9 @@
+package katas.fizzbuzz;
+
+import java.util.function.Function;
+
+public class FizzBuzz implements Function<String, String> {
+  @Override public String apply(String input) {
+    return null;
+  }
+}
---
+++ fizz-buzz/src/test/groovy/katas/fizzbuzz/FizzBuzzSpec.groovy
@@ -0,0 +1,10 @@
+package katas.fizzbuzz
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+class FizzBuzzSpec extends Specification {
+
+  @Subject def function = new FizzBuzz()
+
+}
