1e69c91880b975c47cf6cee734b67ccd648a1813
Rob Fletcher <rfletcher@netflix.com>
2015-07-29 12:27:25 -0500

    reformat

--- vending-machine/src/test/groovy/katas/vending/CoinSpec.groovy
+++ vending-machine/src/test/groovy/katas/vending/CoinSpec.groovy
@@ -2,23 +2,20 @@ package katas.vending
 
 import spock.lang.Specification
 import spock.lang.Unroll
-
 import static katas.vending.Coin.*
 
 class CoinSpec extends Specification {
+  @Unroll
+  def "#coin is printed as '#expected'"() {
+    expect:
+    coin.toString() == expected
 
-    @Unroll
-    def "#coin is printed as '#expected'"() {
-        expect:
-        coin.toString() == expected
-
-        where:
-        coin    | expected
-        PENNY   | "1¢"
-        NICKEL  | "5¢"
-        DIME    | "10¢"
-        QUARTER | "25¢"
-        DOLLAR  | '$1'
-    }
-
+    where:
+    coin    | expected
+    PENNY   | "1¢"
+    NICKEL  | "5¢"
+    DIME    | "10¢"
+    QUARTER | "25¢"
+    DOLLAR  | '$1'
+  }
 }
