8264ab0acd18542a80b00b8e2756b8eb32a575e2
Rob Fletcher <rfletcher@netflix.com>
2015-07-30 11:57:50 -0500

    fizz buzz solution using QuickCheck

--- fizz-buzz/src/main/groovy/katas/fizzbuzz/FizzBuzz.java
+++ fizz-buzz/src/main/groovy/katas/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,13 @@ package katas.fizzbuzz;
 
 public class FizzBuzz {
   public String apply(Integer input) {
-    return null;
+    StringBuilder result = new StringBuilder();
+    if (input % 3 == 0) {
+      result.append("fizz");
+    }
+    if (input % 5 == 0) {
+      result.append("buzz");
+    }
+    return result.length() > 0 ? result.toString() : input.toString();
   }
 }
--- fizz-buzz/src/test/groovy/katas/fizzbuzz/FizzBuzzSpec.groovy
+++ fizz-buzz/src/test/groovy/katas/fizzbuzz/FizzBuzzSpec.groovy
@@ -1,10 +1,50 @@
 package katas.fizzbuzz
 
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Subject
+import spock.lang.Unroll
+import static net.java.quickcheck.generator.PrimitiveGeneratorsIterables.someIntegers
 
 class FizzBuzzSpec extends Specification {
 
   @Subject def function = new FizzBuzz()
+  @Shared def integers = someIntegers()
+
+  @Unroll("fizzbuzz(#n) returns 'fizz'")
+  def "an argument divisible by 3 returns 'fizz'"() {
+    expect:
+    function.apply(n) == "fizz"
+
+    where:
+    n << integers.findAll { it % 3 == 0 && it % 5 != 0 }
+  }
+
+  @Unroll("fizzbuzz(#n) returns 'buzz'")
+  def "an argument divisible by 5 returns 'buzz'"() {
+    expect:
+    function.apply(n) == "buzz"
+
+    where:
+    n << integers.findAll { it % 5 == 0 && it % 3 != 0 }
+  }
+
+  @Unroll("fizzbuzz(#n) returns 'fizzbuzz'")
+  def "an argument divisible by 3 and 5 returns 'fizzbuzz'"() {
+    expect:
+    function.apply(n) == "fizzbuzz"
+
+    where:
+    n << integers.findAll { it % 3 == 0 && it % 5 == 0 }
+  }
+
+  @Unroll("fizzbuzz(#n) returns '#n'")
+  def "any other argument returns itself"() {
+    expect:
+    function.apply(n) == n.toString()
+
+    where:
+    n << integers.findAll { it % 3 != 0 && it % 5 != 0 }
+  }
 
 }
